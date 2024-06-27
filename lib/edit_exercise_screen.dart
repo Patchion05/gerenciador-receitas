901d39630117e61b9ecb1fa29d7af6c947b1205d
Patchion05 <paulojoaocr4@gmail.com>
2024-06-27 20:38:28 -0300

    teste

--- lib/edit_exercise_screen.dart
+++ lib/edit_exercise_screen.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 class EditExerciseScreen extends StatefulWidget {
   final Map<String, dynamic> exercise;
 
-  EditExerciseScreen({required this.exercise});
+  const EditExerciseScreen({super.key, required this.exercise});
 
   @override
   _EditExerciseScreenState createState() => _EditExerciseScreenState();
@@ -32,8 +32,8 @@ class _EditExerciseScreenState extends State<EditExerciseScreen> {
 
   Future<void> _editExercise() async {
     if (_formKey.currentState!.validate()) {
-      final String baseUrl =
-          'https://gerenciador-de-receitas2-default-rtdb.firebaseio.com/';
+      const String baseUrl =
+          'https://gerenciador-de-receitas2-default-rtdb.firebaseio.com/22222222';
       final response = await http.patch(
         Uri.parse('$baseUrl/exercises/${widget.exercise['key']}.json'),
         body: json.encode({
@@ -64,17 +64,18 @@ class _EditExerciseScreenState extends State<EditExerciseScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Editar Receitas'),
+        title: const Text('Editar Receitas'),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: Column(
             children: [
               TextFormField(
                 controller: _typeController,
-                decoration: InputDecoration(labelText: 'Tempo de Receitas'),
+                decoration:
+                    const InputDecoration(labelText: 'Tempo de Receitas'),
                 validator: (value) {
                   if (value!.isEmpty) {
                     return 'Por favor, insira o Tempo de Receitas';
@@ -84,7 +85,7 @@ class _EditExerciseScreenState extends State<EditExerciseScreen> {
               ),
               TextFormField(
                 controller: _durationController,
-                decoration: InputDecoration(labelText: 'Modo de Preparo'),
+                decoration: const InputDecoration(labelText: 'Modo de Preparo'),
                 validator: (value) {
                   if (value!.isEmpty) {
                     return 'Por favor, insira o modo de Preparo';
@@ -94,7 +95,8 @@ class _EditExerciseScreenState extends State<EditExerciseScreen> {
               ),
               TextFormField(
                 controller: _caloriesController,
-                decoration: InputDecoration(labelText: 'Calorias Queimadas'),
+                decoration:
+                    const InputDecoration(labelText: 'Calorias Queimadas'),
                 validator: (value) {
                   if (value!.isEmpty) {
                     return 'Por favor, insira as ';
@@ -104,7 +106,7 @@ class _EditExerciseScreenState extends State<EditExerciseScreen> {
               ),
               TextFormField(
                 controller: _dateController,
-                decoration: InputDecoration(labelText: 'Data'),
+                decoration: const InputDecoration(labelText: 'Data'),
                 validator: (value) {
                   if (value!.isEmpty) {
                     return 'Por favor, insira a data';
@@ -112,10 +114,10 @@ class _EditExerciseScreenState extends State<EditExerciseScreen> {
                   return null;
                 },
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: _editExercise,
-                child: Text('Salvar'),
+                child: const Text('Salvar'),
               ),
             ],
           ),
